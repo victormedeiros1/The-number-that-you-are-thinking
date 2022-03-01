f2ff59c6590239abd6716cacef68cd6bae7eedd8
victormedeiros1 <josevqczt@gmail.com>
2022-03-01 18:44:56 -0300

    feat: Container component

---
+++ src/components/Container/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { ContainerStyles } from "./styles";
+
+export default function Container({ children }) {
+  return <ContainerStyles>{children}</ContainerStyles>;
+}
