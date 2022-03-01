d03a936147d06910c3dfc1cd9b5924bb3ad5d195
victormedeiros1 <josevqczt@gmail.com>
2022-03-01 11:31:50 -0300

    feat: basic configs and 3 screens

---
+++ src/components/Screens/InGame/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InGame() {
+  return <div />;
+}
+
+export default InGame;
---
+++ src/components/Screens/StartGame copy 2/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function StartGame() {
+  return <div />;
+}
+
+export default StartGame;
