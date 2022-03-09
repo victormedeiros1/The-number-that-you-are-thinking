2471661921c962078548001276f218d4acd356a1
victormedeiros1 <josevqczt@gmail.com>
2022-03-08 23:00:10 -0300

    feat: function for resetState

--- src/components/Screens/GameOver/index.js
+++ src/components/Screens/GameOver/index.js
@@ -6,12 +6,18 @@ import { useAttempts } from "../../../context/Attempts";
 import { GameOverContainer } from "./styles";
 function GameOver() {
   const { setScreen } = useScreen();
-  const { attempts } = useAttempts();
+  const { attempts, setAttempts } = useAttempts();
+
+  const resetStates = () => {
+    setScreen("StartGame");
+    setAttempts(0);
+  };
+
   return (
     <GameOverContainer>
       <p>Fim de Jogo</p>
       <p>O algortimo deu {attempts} palpites para acertar</p>
-      <Button onClick={() => setScreen("StartGame")}>Jogar Novamente</Button>
+      <Button onClick={resetStates}>Jogar Novamente</Button>
     </GameOverContainer>
   );
 }
